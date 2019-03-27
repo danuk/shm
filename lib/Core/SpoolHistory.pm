776d803919f2600c0a8086f19b207bc5253f7ded
Daniil Firsov <mail@danuk.ru>
2019-03-27 14:31:17 +0000

    fixup

--- lib/Core/SpoolHistory.pm
+++ lib/Core/SpoolHistory.pm
@@ -6,4 +6,21 @@ use Core::Base;
 
 sub table { return 'spool_history' };
 
+sub structure {
+    my $self = shift;
+    return {
+        spool_id => '?',
+        %{ $self->SUPER::structure },
+    }
+}
+
+sub add {
+    my $self = shift;
+    my %args = @_;
+
+    $args{spool_id} = delete $args{id};
+
+    return $self->SUPER::add( %args );
+}
+
 1;
