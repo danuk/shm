5f36e3fd029a0ac0de53bb717d4c03821b87db24
Daniil Firsov <mail@danuk.ru>
2019-10-11 15:27:12 +0000

    dnk: allow fractional period_cost

--- app/sql/shm/shm_structure.sql
+++ app/sql/shm/shm_structure.sql
@@ -140,7 +140,7 @@ CREATE TABLE `services` (
   `service_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` char(64) NOT NULL,
   `cost` decimal(10,2) DEFAULT NULL,
-  `period_cost` tinyint(4) NOT NULL,
+  `period_cost` decimal(10,2) NOT NULL DEFAULT '0.00',
   `category` char(16) DEFAULT NULL,
   `next` int(11) DEFAULT '0',
   `opt` tinyint(4) DEFAULT NULL,
