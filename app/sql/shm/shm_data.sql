9a7e718a14cd96226a6ae44fba082ed49e768be9
Daniil Firsov <mail@danuk.ru>
2020-01-13 15:54:19 +0000

    dnk: fixup shm_data servers_group

--- app/sql/shm/shm_data.sql
+++ app/sql/shm/shm_data.sql
@@ -4,7 +4,7 @@ INSERT INTO `users` VALUES
 (1,0,'admin','0df78fa86a30eca0a918fdd21a94e238133ce7ab',0,NOW(),NULL,0,0,0,0.00,NULL,NULL,0,0,1,0,'Admin',0,0.00,NULL,NULL,NULL);
 
 INSERT INTO `servers_groups` VALUES
-(1,'Основная','random',NULL);
+(1,'Основная','ssh','random',NULL);
 
 INSERT INTO `templates` VALUES (1,'web_tariff_create','Создание тарифа хостинга','Здравствуйте {{ user.full_name }}\n\nВы зарегистрировали новую услугу: {{ us.service.name }}\n\nДата истечения услуги: {{ us.expired }}\n\nСтоимость услуги: {{ us.service.cost }} руб.\n\nХостинг сайтов:\nХост: {{ child(\'web\').server.settings.host_name }}\nЛогин: {{ child(\'web\').settings.login }}\nПароль: {{ child(\'web\').settings.password }}\n\nЖелаем успехов.',NULL);
 
