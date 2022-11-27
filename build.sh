3a580b060a0a72cbfbc1a4cdbd1a040b626f16f4
Daniil Firsov <daniil.firsov@pimpay.ru>
2022-11-27 22:39:47 +0300

    dnk: new build.sh with versions support

--- build.sh
+++ build.sh
@@ -1,6 +1,33 @@
-docker build -t danuk/shm-api --target api .
-docker push danuk/shm-api
+#!/bin/bash
 
-docker build -t danuk/shm --target core .
-docker push danuk/shm
+VERSION=$(git tag --points-at | head -n1)
+
+if [ -z "$VERSION" ]; then
+    echo "Error: tag required"
+fi
+
+VERSION_MINOR=$(echo $VERSION | cut -d '.' -f 1,2)
+
+function build_and_push {
+    TAGS=(
+        danuk/shm-$1:$VERSION
+        danuk/shm-$1:$VERSION_MINOR
+        danuk/shm-$1
+    )
+
+    docker build \
+        $(printf " -t %s" "${TAGS[@]}") \
+        --target $1 .
+
+    for TAG in ${TAGS[*]}; do
+        docker push $TAG
+    done
+}
+
+# Build API
+build_and_push api
+
+# Build Core
+echo -n "$VERSION" > app/version
+build_and_push core
 
