e4114a62f1e13d5ec4d29fa0a80d2afc181afa4b
Daniil Firsov <daniil.firsov@pimpay.ru>
2023-10-29 21:32:47 +0300

    dnk: add SHM_TEMPLATE for setWebhook.sh

--- scripts/telegram/setWebhook.sh
+++ scripts/telegram/setWebhook.sh
@@ -2,12 +2,16 @@
 
 TOKEN=""
 URL="bill.DOMAIN.ru"
+SHM_TEMPLATE="telegram_bot"
 
 curl https://api.telegram.org/bot${TOKEN}/deleteWebhook?drop_pending_updates=True
 
 curl -X POST \
     -H 'content-type: application/json' \
     https://api.telegram.org/bot${TOKEN}/setWebhook \
-    -d '{"url": "https://'$URL'/shm/v1/telegram/bot" }"'
-
+    -d "
+{
+    \"url\": \"https://$URL/shm/v1/telegram/bot/${SHM_TEMPLATE}\",
+    \"allowed_updates\": []
+}"
 
