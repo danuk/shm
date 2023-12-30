b4ca4fa93cd2c4494db034255cbb5ddd973f32c9
Daniil Firsov <daniil.firsov@pimpay.ru>
2023-12-30 19:57:43 +0300

    dnk: add transport `Local`

---
+++ app/lib/Core/Transport/Local.pm
@@ -0,0 +1,38 @@
+package Core::Transport::Local;
+
+use parent 'Core::Base';
+
+use v5.14;
+use utf8;
+use Core::Base;
+use Core::Const;
+
+sub send {
+    my $self = shift;
+    my $task = shift;
+
+    my $template_id = $task->event_settings->{template_id};
+
+    my $template = get_service('template', _id => $template_id );
+    unless ( $template ) {
+        return undef, {
+            error => "template with id `$template_id` not found",
+        }
+    }
+
+    my %settings = (
+        %{ $template->get_settings || {} },
+        %{ $task->event_settings },
+    );
+
+    my $content = $template->parse(
+        $task->settings->{user_service_id} ? ( usi => $task->settings->{user_service_id} ) : (),
+        task => $task,
+    );
+
+    return SUCCESS, {
+        result => $content,
+    };
+}
+
+1;
