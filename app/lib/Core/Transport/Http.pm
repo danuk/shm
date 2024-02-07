bf20a22e0f8db53c79aac62464c9928e6df41f44
Daniil Firsov <daniil.firsov@pimpay.ru>
2024-02-07 12:59:31 +0300

    dnk: hotifx HTTP transport for correct determine template_id in User Tasks

--- app/lib/Core/Transport/Http.pm
+++ app/lib/Core/Transport/Http.pm
@@ -27,7 +27,10 @@ sub send {
         };
     }
 
-    my $template_id = $task->event_settings->{template_id} || $server{settings}->{template_id};
+
+    my $template_id =   $task->event_settings->{template_id} ||
+                        $task->settings->{template_id} ||
+                        $server{settings}->{template_id};
 
     my $template = get_service('template', _id => $template_id );
     unless ( $template ) {
