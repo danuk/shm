110d09c4ce0c1000f112e0df13705fff10950b2b
Daniil Firsov <daniil.firsov@pimpay.ru>
2024-04-15 21:27:50 +0300

    Revert "dnk: use REPO for publish images"
    This reverts commit e7b3cd91ef31665545aa97cb058b9a8f3a7e2b3f.

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-REPO="cr.yandex/crp53phlntopuut9qqq2"
-
 function build_and_push {
-    TAGS=("$REPO/shm-$1:latest")
+    TAGS=("danuk/shm-$1:latest")
 
     [ -z "$VERSION" ] && VERSION=$(git tag --points-at | head -n1)
     if [ "$VERSION" ]; then
-        #TAGS+=("$REPO/shm-$1:$VERSION")
+        #TAGS+=("danuk/shm-$1:$VERSION")
 
         VERSION_MINOR=$(echo $VERSION | cut -d '.' -f 1,2)
-        TAGS+=("$REPO/shm-$1:$VERSION_MINOR")
+        TAGS+=("danuk/shm-$1:$VERSION_MINOR")
     fi
 
     docker build \
