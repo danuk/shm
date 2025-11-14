10f4c67a0cd08efa966164823d92d82a03294d64
Daniil Firsov <daniil.firsov@pimpay.ru>
2025-11-14 11:48:44 +0300

    dnk add TELEGRAM secret token

--- scripts/telegram/setWebhook.sh
+++ scripts/telegram/setWebhook.sh
@@ -4,6 +4,11 @@ TOKEN=""
 URL="bill.DOMAIN.ru"
 SHM_TEMPLATE="telegram_bot"
 
+# Change me!!!
+# You can generate SECRET_TOKEN with the following command: openssl rand -hex 32
+# 1-256 characters. Only characters A-Z, a-z, 0-9, _ and - are allowed
+SECRET=""
+
 curl https://api.telegram.org/bot${TOKEN}/deleteWebhook?drop_pending_updates=True
 
 cat << EOF | curl -X POST \
@@ -12,6 +17,7 @@ cat << EOF | curl -X POST \
     -d @-
 {
     "url": "https://${URL}/shm/v1/telegram/bot/${SHM_TEMPLATE}",
+    "secret_token": "${SECRET}",
     "allowed_updates": [
         "message",
         "inline_query",
