7a430d44df617f22af45e7e4b8464646f0472019
Daniil Firsov <daniil.firsov@pimpay.ru>
2026-03-26 18:36:59 +0300

    dnk: улучшение миграции

--- app/bin/migrations/2.8.0.sql
+++ app/bin/migrations/2.8.0.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `users` ADD COLUMN `login2` char(64) DEFAULT NULL AFTER `login`;
-UPDATE `users` SET `login2` = CONCAT('@', `settings` ->> '$.telegram.user_id') WHERE `settings` ->> '$.telegram.user_id' IS NOT NULL;
+UPDATE `users` SET `login2` = CONCAT('@', `settings` ->> '$.telegram.user_id') WHERE `settings` ->> '$.telegram.user_id' IS NOT NULL AND `login` != CONCAT('@', `settings` ->> '$.telegram.user_id');
 ALTER TABLE `users` ADD UNIQUE KEY `users_uniq_login2` (`login2`);
