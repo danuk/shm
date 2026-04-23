5d33a6c8797b58907ce7cda180295d8daca28569
Daniil Firsov <daniil.firsov@pimpay.ru>
2026-04-23 14:15:52 +0300

    dnk: правки времени жизни коннектов MyISAM

--- app/lib/Core/SpoolHistory.pm
+++ app/lib/Core/SpoolHistory.pm
@@ -5,7 +5,7 @@ use parent 'Core::Spool';
 use Core::Base;
 
 sub table { return 'spool_history' };
-sub dbh { my $self = shift; $self->{_autocommit_dbh} ||= $self->dbh_new( AutoCommit => 1 ); return $self->{_autocommit_dbh} };
+sub dbh { my $self = shift; $self->{_autocommit_dbh} ||= $self->dbh_new( AutoCommit => 1, InactiveDestroy => 0 ); return $self->{_autocommit_dbh} };
 
 sub structure {
     my $self = shift;
