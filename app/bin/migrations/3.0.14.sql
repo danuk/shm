ab5c5d5baa0a718db9cf813479477837b731611b
Daniil Firsov <daniil.firsov@pimpay.ru>
2026-07-30 12:54:59 +0300

    dnk: LOGS - добавлено логирование API в БД

---
+++ app/bin/migrations/3.0.14.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `logs_api` (
+  `user_id` int(11) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `url` varchar(512) NOT NULL,
+  `method` varchar(10) NOT NULL,
+  `args` json DEFAULT NULL,
+  `descr` char(128) DEFAULT NULL,
+  `duration` int(11) unsigned NOT NULL DEFAULT '0',
+  `response_code` smallint(5) unsigned NOT NULL DEFAULT '0',
+  KEY `idx_logs_api_user_id` (`user_id`),
+  KEY `idx_logs_api_date` (`date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
